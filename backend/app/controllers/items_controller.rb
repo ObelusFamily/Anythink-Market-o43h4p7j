9ba5f6d561080db6c5e50a252eae45c81f510c51
Alfredx48 <110496237+Alfredx48@users.noreply.github.com>
2023-02-18 01:10:05 +0000

    add Dall-e Images

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative "../../lib/event"
+require "openai"
 include Event
 
 class ItemsController < ApplicationController
@@ -51,9 +52,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+    client = OpenAI::Client.new(access_token: "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")
+    response = client.images.generate(parameters: { prompt: "A baby sea otter cooking pasta wearing a hat of some sort", size: "256x256" })
     @item = Item.new(item_params)
     @item.user = current_user
-
+    if @item.image.blank?
+      @item.image = response.dig("data", 0, "url")
+      @item.save!
+    end
     if @item.save
       sendEvent("item_created", { item: item_params })
       render :show
